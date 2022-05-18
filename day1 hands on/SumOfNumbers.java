15d69a6a3b59fb7b5211c8c8e86973d5f5b9f370
Surya1824 <suryateja5347@tcs.com>
2022-05-18 15:29:19 +0000

    Day1 Java Coding

---
+++ day1 hands on/SumOfNumbers.java
@@ -0,0 +1,12 @@
+
+public class SumOfNumbers {
+
+	public static void main(String[] args) {
+		int num1 =10;
+		int num2 =40;
+		int num3 =77;
+		int result = num1+num2+num3;
+		System.out.println("Sum of the Numbers" + result);
+	}
+
+}
---
+++ day1 hands on/SumOfNumbers2.java
@@ -0,0 +1,20 @@
+class SumLogicNumbers{
+	
+	void logic(int num1, int num2) {
+		int result;
+		result = num1+num2;
+		System.out.println("Sum of the Numbers " + result);
+		
+	}
+	}
+
+
+public class SumOfNumbers2 {
+
+	public static void main(String[] args) {
+		SumLogicNumbers sum = new SumLogicNumbers();
+		sum.logic(10, 20);
+
+	}
+
+}
