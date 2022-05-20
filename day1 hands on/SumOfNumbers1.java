4311776d75ff638f9dbc7363c93d8e9d3fe4648f
Surya1824 <suryateja5347@tcs.com>
2022-05-20 15:56:40 +0000

    three days hands on

---
+++ day1 hands on/SumOfNumbers1.java
@@ -0,0 +1,22 @@
+
+class SumLogic{
+	
+	void logic() {
+		int num1 =10;
+		int num2 =40;
+		int num3 =77;
+		int result = num1+num2+num3;
+		System.out.println("Sum of the Numbers " + result);
+	}
+}
+
+
+public class SumOfNumbers1 {
+
+	public static void main(String[] args) {
+		SumLogic sum = new SumLogic();
+		sum.logic();
+		
+	}
+
+}
